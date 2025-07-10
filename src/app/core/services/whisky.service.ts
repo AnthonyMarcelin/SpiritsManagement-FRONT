c1d72e2a650dc2015ddaab4b0666a804e70f22b8
Anthony Marcelin <anthony.marcelin13@gmail.com>
2025-07-11 00:48:57 +0200

    add deleted files

---
+++ src/app/core/services/whisky.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Whisky } from '../../models/whisky.interface';
+import { ApiService } from './api.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class WhiskyService {
+  constructor(private apiService: ApiService) {}
+
+  getAllWhisky(): Observable<Whisky[]> {
+    return this.apiService.get<Whisky[]>('whisky');
+  }
+
+  getWhiskyById(id: string): Observable<Whisky> {
+    return this.apiService.get<Whisky>(`whisky/${id}`);
+  }
+}

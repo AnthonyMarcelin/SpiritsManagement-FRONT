6f475520206894ac4fb9a5fc5068dd3c0040b467
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-07-16 18:32:45 +0200

    feat/register

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterModule],
+  standalone: true,
   templateUrl: './register.html',
-  styleUrl: './register.scss'
+  styleUrl: './register.scss',
 })
 export class Register {
+  pseudo = '';
+  firstName = '';
+  lastName = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  error = '';
 
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
+
+  onSubmit() {
+    if (this.password !== this.confirmPassword) {
+      this.error = 'Les mots de passe ne correspondent pas';
+      return;
+    }
+    this.auth
+      .register({
+        pseudo: this.pseudo,
+        firstname: this.firstName,
+        lastname: this.lastName,
+        email: this.email,
+        password: this.password,
+        isAdmin: false,
+      })
+      .subscribe({
+        next: () => {
+          console.log('Registration successful');
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          console.error('Registration failed', err);
+          this.error = "Erreur lors de l'inscription";
+        },
+      });
+    console.log('Registration successful for:', this.email);
+  }
 }

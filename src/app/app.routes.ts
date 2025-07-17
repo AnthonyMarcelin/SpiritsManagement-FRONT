5c287ec622e887f6701468384eaf7f20f0f10fea
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-07-17 22:48:33 +0200

    feat/ add admin condition and logout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,10 @@ import { NotFound } from './features/not-found/not-found';
 import { WhiskyList } from './features/whisky/whisky-list/whisky-list';
 
 export const routes: Routes = [
-  { path: '', component: Homepage }, // default
+  { path: '', component: Homepage },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'mainpage', component: Mainpage },
-  { path: 'whisky', component: WhiskyList, canActivate: [authGuard] }, // Page des whiskies
-  { path: '**', component: NotFound }, // Toutes les autres (404)
+  { path: 'mainpage', component: Mainpage, canActivate: [authGuard] },
+  { path: 'whisky', component: WhiskyList, canActivate: [authGuard] },
+  { path: '**', component: NotFound },
 ];
--- src/app/shared/components/layout/navbar/navbar.ts
+++ src/app/shared/components/layout/navbar/navbar.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar implements OnInit {
+  isAdmin = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.auth.getMe().subscribe({
+      next: (user) => {
+        this.isAdmin = !!user.isAdmin;
+      },
+      error: () => {
+        this.isAdmin = false;
+      },
+    });
+  }
+
+  logout() {
+    this.auth.logout().subscribe({
+      next: () => {
+        this.router.navigate(['']);
+      },
+      error: () => {
+        // Optionnel : afficher une erreur ou forcer la déconnexion côté front
+        this.router.navigate(['/login']);
+      },
+    });
+  }
+}

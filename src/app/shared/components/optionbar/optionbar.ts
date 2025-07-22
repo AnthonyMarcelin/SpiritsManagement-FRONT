0e72d105d4e1e0ee7a4db5f53a121909cc252026
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-07-22 22:57:22 +0200

    add shared components

---
+++ src/app/shared/components/optionbar/optionbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-optionbar',
+  imports: [],
+  templateUrl: './optionbar.html',
+  styleUrl: './optionbar.scss'
+})
+export class Optionbar {
+
+}

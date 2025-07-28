2873d0e9d02efd164673674bbde2365cf619bb1a
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-07-28 19:38:36 +0200

    add rgpd

---
+++ src/app/shared/components/rgpd/rgpd.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FooterComponent } from '../footer/footer';
+
+@Component({
+  selector: 'app-rgpd',
+  templateUrl: './rgpd.html',
+  styleUrls: ['./rgpd.scss'],
+  standalone: true,
+  imports: [FooterComponent],
+})
+export class RGPDComponent {
+  constructor(private router: Router) {}
+  goHome() {
+    this.router.navigate(['/']);
+  }
+}
